56faeacaa973da439a597952ff0e22fedf1773dd
JannetCT <jchavez@fertinyect.com>
2026-05-26 13:48:21 +0200

    fix: forzar prompt consent en OAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -54,7 +54,7 @@ export function useAuth() {
   function login() {
     const scope = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid'
     const redirectUri = window.location.origin
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${encodeURIComponent(scope)}`
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=tokenresponse_type=token&scopeprompt=consent&scope=${encodeURIComponent(scope)}`
     window.location.href = url
   }
 
